4ac410b6dcdd80263cc9f828728c3195228f1082
Victor Lupascu <sinajeen@gmail.com>
2021-08-15 00:39:16 -0500

    setup sessions, cookies and handlebars

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // load dependecies
 const express = require('express');
 const path = require('path');
-
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 // create server/assign a port
 const app = express();
@@ -9,7 +10,25 @@ const PORT = process.env.PORT || 3001;
 
 // create connection to database
 const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// setting up cookies
+const sess = {
+     secret: process.env.SECRET,
+     cookie: { },
+     resave: false,
+     saveUninitialized: true,
+     store: new SequelizeStore({
+          db: sequelize
+     })
+};
+
+app.use(session(sess));
+
+// setting up handlebars as template engine
+const hbs = exphbs.create({ });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // add midlleware
 app.use(express.json());
