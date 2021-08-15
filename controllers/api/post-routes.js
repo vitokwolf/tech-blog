efb4a0cb7ea2bf0bcd217d9374991f144c278a7d
Victor Lupascu <sinajeen@gmail.com>
2021-08-15 00:14:10 -0500

    add get all posts route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,6 +2,29 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
 
 // get all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // get single post by id
 
