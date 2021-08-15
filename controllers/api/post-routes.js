c322cf1e6b18bf99c8f9d58b927999935167b355
Victor Lupascu <sinajeen@gmail.com>
2021-08-15 00:10:22 -0500

    setup post api routes file

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+
+// get all posts
+
+// get single post by id
+
+// create post
+
+// update post
+
+// delete post
+
+
+module.exports = router;
