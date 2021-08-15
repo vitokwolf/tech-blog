cdf18b6251d78c46979a79ce29fff77d7edbf9ba
Victor Lupascu <sinajeen@gmail.com>
2021-08-14 23:06:09 -0500

    initial server setup

---
+++ server.js
@@ -0,0 +1,28 @@
+// load dependecies
+const express = require('express');
+const path = require('path');
+
+
+// create server/assign a port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// create connection to database
+const sequelize = require("./config/connection");
+
+
+// add midlleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// setup the server to use routes from controlers
+// app.use(require('./controllers/'));
+
+// sync database and the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`
+                                        ========================================
+                                             Now listening on PORT: ${PORT}
+                                        ========================================`));
+});
