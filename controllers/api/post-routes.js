ae9fd50b282155ee771602accdb24771eab55a10
Victor Lupascu <sinajeen@gmail.com>
2021-08-15 00:51:17 -0500

    edit post routes to check for auth

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
 
 // get all posts
 router.get('/', (req, res) => {
@@ -59,11 +61,11 @@ router.get('/:id', (req, res) => {
 });
 
 // create post
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
@@ -73,7 +75,7 @@ router.post('/', (req, res) => {
 });
 
 // update post
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update(
         {
             title: req.body.title
@@ -98,7 +100,7 @@ router.put('/:id', (req, res) => {
 });
 
 // delete post
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id
