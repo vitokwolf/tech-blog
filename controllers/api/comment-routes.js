da778cf01d8ca9462a01e0d99584e8594d096727
Victor Lupascu <sinajeen@gmail.com>
2021-08-15 00:30:25 -0500

    add create comment route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -31,7 +31,18 @@ router.get('/:id', (req, res) => {
 });
 
 // create comment
-
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
 
 // update comment
 
