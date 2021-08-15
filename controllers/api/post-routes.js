1ef26ea8a84c95dc9c4a1056092e2b0666e3c096
Victor Lupascu <sinajeen@gmail.com>
2021-08-15 00:21:07 -0500

    add delete post route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -98,6 +98,23 @@ router.put('/:id', (req, res) => {
 });
 
 // delete post
-
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
