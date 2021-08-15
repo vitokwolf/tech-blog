578280b23015f85db2f5ead022e66534ecfb8501
Victor Lupascu <sinajeen@gmail.com>
2021-08-15 00:25:03 -0500

    add get all comments route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,7 +2,14 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
 // get all comments
-
+router.get('/', (req, res) => {
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // get single comment
 
