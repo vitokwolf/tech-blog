3935ae159a532678491caeff0580547b6d75a176
Victor Lupascu <sinajeen@gmail.com>
2021-08-15 00:23:20 -0500

    initial comment routes setup

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+// get all comments
+
+
+// get single comment
+
+
+// create comment
+
+
+// update comment
+
+
+// delete comment
+
+module.exports = router;
