9277afb683812b11bcb5a81f76a8d9570df8eb62
Victor Lupascu <sinajeen@gmail.com>
2021-08-15 00:44:31 -0500

    add login route, update create user route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -48,12 +48,47 @@ router.post('/', (req, res) => {
         password: req.body.password
     })
         .then(dbUserData => {
+            // after creating a user, it logs in automatically
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
                 res.json(dbUserData);
+            });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+// login route
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username,
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
 });
 
 // update user 
