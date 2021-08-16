1b328b420f691771bb970f76ab37c6e6e8a75d24
Victor Lupascu <sinajeen@gmail.com>
2021-08-16 00:21:11 -0500

    add login, signup, logout

---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('user-login').value.trim();
+    const password = document.getElementById('password-login').value.trim();
+
+    console.log(username)
+    console.log(password)
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    };
+};
+
+document.getElementById('login').addEventListener('submit', loginFormHandler);
+
---
+++ public/js/signup.js
@@ -0,0 +1,27 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username-signup').value.trim();
+    const password = document.getElementById('password-signup').value.trim();
+
+    console.log(username)
+    console.log(password)
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard')
+        } else {
+            alert(response.statusText);
+        }
+    };
+};
+
+document.getElementById('signup').addEventListener('submit', signupFormHandler);
